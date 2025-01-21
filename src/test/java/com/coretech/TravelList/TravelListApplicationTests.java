05ad5b5f49b2399cc7d309ed58b09c6536ee67b8
Brena Soares <brehcore@gmail.com>
2025-01-21 16:51:00 -0300

    Iniciando o projeto: construindo a base

---
+++ src/test/java/com/coretech/TravelList/TravelListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coretech.TravelList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
