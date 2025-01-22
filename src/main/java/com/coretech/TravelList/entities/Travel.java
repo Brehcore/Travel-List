82acedc7a71d38721d58d4fc339a2f151bbaaf26
Brena Soares <brehcore@gmail.com>
2025-01-22 18:21:48 -0300

    Inserindo informaçÃões na tabela e testando no H2

--- src/main/java/com/coretech/TravelList/entities/Travel.java
+++ src/main/java/com/coretech/TravelList/entities/Travel.java
@@ -13,9 +13,13 @@ public class Travel {
     private Long id;
 
     private String title;
+    private String city;
+    private String state;
     private String category;
     private String monthToVisit;
     private String imgUrl;
+
+    @Column(name = "description", length = 1000)
     private String description;
 
     public Travel() {
@@ -25,6 +29,8 @@ public class Travel {
     public Travel(Long id, String title, String category, String monthToVisit, String imgUrl, String description) {
         this.id = id;
         this.title = title;
+        this.city = city;
+        this.state = state;
         this.category = category;
         this.monthToVisit = monthToVisit;
         this.imgUrl = imgUrl;
@@ -45,6 +51,22 @@ public class Travel {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String estado) {
+        this.state = state;
     }
 
     public String getCategory() {
